6e3f6aadcd9eb24d35a0c2c7be8df357c63af881
Miguel Obeid <mobeid@itba.edu.ar>
2024-10-23 13:22:46 -0300

    Update 02_InfoMed_TP5_GiorgioniObeid.sql

--- Ejercicio 2/02_InfoMed_TP5_GiorgioniObeid.sql
+++ Ejercicio 2/02_InfoMed_TP5_GiorgioniObeid.sql
@@ -1,2 +1,8 @@
+ALTER TABLE pacientes
+ADD COLUMN IF NOT EXISTS edad INT;
+
 UPDATE pacientes
-SET edad = EXTRACT(YEAR FROM AGE(CURRENT_DATE, fecha_nacimiento));}
+SET edad = EXTRACT(YEAR FROM AGE(CURRENT_DATE, fecha_nacimiento));
+
+CREATE INDEX IF NOT EXISTS idx_edades
+ON pacientes (edad); 
